f096ddd3c2cb6147446a3ef4aa3bb12bd8062ae6
Yusuke Ito <novi.mad@gmail.com>
2017-05-22 12:16:28 +0900

    add demo

--- Tests/Word2VecTests/word2vec_swiftTests.swift
+++ Tests/Word2VecTests/word2vec_swiftTests.swift
@@ -7,6 +7,13 @@ class word2vec_swiftTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(word2vec_swift().text, "Hello, World!")
     }
+    
+    func testLoadModel() {
+        
+        let distance = Distance(modelPath: "/Users/ito/ExtProjs/Word2Vec-iOS/Word2Vec-iOS/res/out.bin")
+        
+        distance.calcDistance(words: "cat dog", limit: 120)
+    }
 
 
     static var allTests = [
